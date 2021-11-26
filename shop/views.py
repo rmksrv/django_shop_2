1e010ac10cd98a74f7d5f92951ad0fb6de1c53fa
Roman Kosarev <rmksrv@outlook.com>
2021-11-27 02:12:45 +0300

    base views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from .models import Category, Product
+
+
+def product_list(request, category_slug: Optional[str] = None):
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+    return HttpResponse(products)
